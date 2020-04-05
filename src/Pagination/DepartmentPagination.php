08db6a68ad0694a1343291dc2acb90c38946dd09
Craig Rayner <craig@craigrayner.com>
2020-04-05 16:53:36 +1000

    Tweak
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- src/Pagination/DepartmentPagination.php
+++ src/Pagination/DepartmentPagination.php
@@ -67,7 +67,7 @@ class DepartmentPagination extends AbstractPaginationManager
 
         $action = new PaginationAction();
         $action->setTitle('Edit')
-            ->setAClass('')
+            ->setAClass('thickbox p-3 sm:p-0')
             ->setColumnClass('column p-2 sm:p-3')
             ->setSpanClass('fas fa-edit fa-fw fa-1-5x text-gray-700')
             ->setRoute('departments__edit')
@@ -76,7 +76,7 @@ class DepartmentPagination extends AbstractPaginationManager
 
         $action = new PaginationAction();
         $action->setTitle('Delete')
-            ->setAClass('')
+            ->setAClass('thickbox p-3 sm:p-0')
             ->setColumnClass('column p-2 sm:p-3')
             ->setSpanClass('far fa-trash-alt fa-fw fa-1-5x text-gray-700')
             ->setRoute('departments__delete')
--- src/Pagination/DepartmentStaffPagination.php
+++ src/Pagination/DepartmentStaffPagination.php
@@ -55,7 +55,7 @@ class DepartmentStaffPagination extends AbstractPaginationManager
 
         $action = new PaginationAction();
         $action->setTitle('Delete')
-            ->setAClass('')
+            ->setAClass('thickbox p-3 sm:p-0')
             ->setColumnClass('column p-2 sm:p-3')
             ->setSpanClass('far fa-trash-alt fa-fw fa-1-5x text-gray-700')
             ->setRoute('departments__delete_staff')
